a5fe55f10ee5577410f9e9db27d1d0c87098256c
dgadsden <99849542+dgadsden@users.noreply.github.com>
2024-10-28 15:34:33 -0400

    Initial Commit

---
+++ StoryHunters_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryHunters_App
+//
+//  Created by Dejah Gadsden on 10/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoryHunters_AppTests/StoryHunters_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryHunters_AppTests.swift
+//  StoryHunters_AppTests
+//
+//  Created by Dejah Gadsden on 10/28/24.
+//
+
+import Testing
+@testable import StoryHunters_App
+
+struct StoryHunters_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
